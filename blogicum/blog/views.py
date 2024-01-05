41586d231d0a4e00dc86104bb3314c0829971b3d
Bulat Ayupov <sulamar66@gmail.com>
2024-01-05 21:45:26 +0500

    Views.py code short with mixins

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -64,38 +64,32 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         )
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostBaseMixin(LoginRequiredMixin):
+    """Базовый класс для работы с постами."""
+
+    model = Post
+    pk_url_kwarg = 'post_pk'
+
+    def dispatch(self, request, *args, **kwargs):
+        instance = get_object_or_404(
+            Post.post_fk_joined_query.all(),
+            pk=kwargs['post_pk']
+        )
+        if instance.author != request.user:
+            return redirect(instance)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class PostUpdateView(PostBaseMixin, UpdateView):
     """Редактирование обьекта поста его автором."""
 
-    model = Post
     form_class = PostForm
-    pk_url_kwarg = 'post_pk'
-
-    def dispatch(self, request, *args, **kwargs):
-        instance = get_object_or_404(
-            Post.post_fk_joined_query.all(),
-            pk=kwargs['post_pk']
-        )
-        if instance.author != request.user:
-            return redirect(instance)
-        return super().dispatch(request, *args, **kwargs)
 
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(PostBaseMixin, DeleteView):
     """Удаление обьекта поста его автором."""
 
-    model = Post
-    pk_url_kwarg = 'post_pk'
     template_name = 'blog/post_form.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        instance = get_object_or_404(
-            Post.post_fk_joined_query.all(),
-            pk=kwargs['post_pk']
-        )
-        if instance.author != request.user:
-            return redirect(instance)
-        return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
         return reverse_lazy(
@@ -127,13 +121,18 @@ class CategoryDetailView(DetailView):
         return context
 
 
-class ProfileDetailView(DetailView):
-    """Информация о пользователе и показ его публикаций."""
+class ProfileBaseMixin:
+    """Базовый класс для работы с профилем пользователя."""
 
     model = get_user_model()
-    template_name = 'blog/profile.html'
     slug_url_kwarg = 'username'
     slug_field = 'username'
+
+
+class ProfileDetailView(ProfileBaseMixin, DetailView):
+    """Информация о пользователе и показ его публикаций."""
+
+    template_name = 'blog/profile.html'
     profile_posts = None
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
@@ -155,19 +154,17 @@ class ProfileDetailView(DetailView):
         return context
 
 
-class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, ProfileBaseMixin, UpdateView):
     """Редактирование обьекта пользователя."""
 
-    model = get_user_model()
     template_name = 'blog/user_edit.html'
-    slug_url_kwarg = 'username'
-    slug_field = 'username'
     fields = ['username', 'first_name', 'last_name', 'email']
 
-    def dispatch(self, request, *args, **kwargs):
-        if self.get_object() != request.user:
-            return redirect('blog:profile', kwargs['username'])
-        return super().dispatch(request, *args, **kwargs)
+    def get_queryset(self):
+        """Пользователь может редактировать только свой профиль."""
+        return self.model.objects.filter(
+            username=self.request.user.username
+        )
 
     def get_success_url(self):
         return reverse_lazy(
@@ -176,17 +173,27 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
         )
 
 
+class CommentBaseMixin(LoginRequiredMixin):
+    """Базовый класс для работы с комментариями."""
+
+    model = Comment
+
+    def get_queryset(self):
+        """Пользователь может работать только со своими комментариями."""
+        return self.model.objects.filter(
+            post__pk=self.kwargs['post_pk'],
+            author=self.request.user
+        )
+
+
 class CommentCreateView(LoginRequiredMixin, CreateView):
     """Создание обьекта комментария к посту."""
 
     post_obj = None
     model = Comment
-    pk_url_kwarg = 'post_pk'
     form_class = CommentForm
 
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('/auth/login/')
         self.post_obj = get_object_or_404(
             Post.published_posts_query.all(),
             pk=kwargs['post_pk']
@@ -199,52 +206,21 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
+class CommentUpdateView(CommentBaseMixin, UpdateView):
     """Редактирование обьекта комментария его автором."""
 
-    model = Comment
     form_class = CommentForm
     pk_url_kwarg = 'comment_pk'
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('/auth/login/')
-        get_object_or_404(
-            Post.published_posts_query.all(),
-            pk=kwargs['post_pk'],
-        )
-        get_object_or_404(
-            Comment,
-            pk=kwargs['comment_pk'],
-            author=request.user
-        )
-        return super().dispatch(request, *args, **kwargs)
 
-
-class CommentDeleteView(LoginRequiredMixin, DeleteView):
+class CommentDeleteView(CommentBaseMixin, DeleteView):
     """Удаление обьекта комментария его автором."""
 
-    post_obj = None
-    model = Comment
     pk_url_kwarg = 'comment_pk'
     template_name = 'blog/comment_form.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('/auth/login/')
-        self.post_obj = get_object_or_404(
-            Post.published_posts_query.all(),
-            pk=kwargs['post_pk'],
-        )
-        get_object_or_404(
-            Comment,
-            pk=kwargs['comment_pk'],
-            author=request.user
-        )
-        return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
         return reverse_lazy(
             'blog:post_detail',
-            kwargs={'post_pk': self.post_obj.pk}
+            kwargs={'post_pk': self.object.post.pk}
         )
